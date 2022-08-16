f0749ad9cb60efa35ae989453be049070528d8bc
arif99050 <arifulislam99050@gmail.com>
2022-08-16 20:17:27 +0600

    os module

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const path=require('path')
+const os=require('os')
+
+// path module 
 const myPath="/Node/core-modules/index.js"
 console.log(path.basename(myPath))
 console.log(path.dirname(myPath))
+
+
+// Os module 
+console.log(os.platform())
